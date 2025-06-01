5b429f455b29f6566f420619a8d5ff6462621aec
Moe-Htetb <moehtetoo2005@gmail.com>
2025-06-01 21:34:47 +0630

    fix: cookie not remove

--- server/src/controllers/logout.ts
+++ server/src/controllers/logout.ts
@@ -3,10 +3,13 @@ import asyncHandler from "../utils/asyncHandler";
 
 export const logoutController = asyncHandler(
   async (req: Request, res: Response) => {
-    res.cookie("token", "", {
+    res.clearCookie("token", {
       httpOnly: true,
-      expires: new Date(0),
+      secure: process.env.NODE_ENV === "production", // Cookie only over HTTPS in production
+      sameSite: "none", // or "lax" depending on frontend/backend domain setup
+      // path: "/", // Ensure path matches the one used when setting the cookie
     });
-    res.status(200).json({ message: "User Logut" });
+
+    res.status(200).json({ message: "User Logout" });
   }
 );
